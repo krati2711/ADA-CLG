7d20a8fa13899e46e33c722242960deb7e3f1cc6
krati2711 <kratigoel.cs24@bmsce.ac.in>
2026-06-06 16:51:15 +0530

    Update KnapsackDP.c

--- KnapsackDP.c
+++ KnapsackDP.c
@@ -1,56 +1,55 @@
 #include <stdio.h>
-int knapsack(int W, int wt[], int val[], int n) {
-    int dp[n+1][W+1];
-    for (int i = 0; i <= n; i++) {
-        for (int w = 0; w <= W; w++) {
-            if (i == 0 || w == 0) {
-                dp[i][w] = 0;
-            }
-            else if (wt[i-1] <= w) {
-                int include = val[i-1] + dp[i-1][w - wt[i-1]];
-                int exclude = dp[i-1][w];
-                dp[i][w] = (include > exclude) ? include : exclude;
-            }
-            else {
-                dp[i][w] = dp[i-1][w];
-            }
-        }
-    }
-    int selected[n];
-    for (int i = 0; i < n; i++)
-        selected[i] = 0;
-    int i = n;
-    int w = W;
-    while (i > 0 && w > 0) {
-        if (dp[i][w] != dp[i-1][w]) {
-            selected[i-1] = 1;
-            w = w - wt[i-1];
-        }
-        i--;
-    }
-    printf("\nItems included in Knapsack:\n");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", selected[i]);
-    }
-    printf("\n");
-    return dp[n][W];
+
+int max(int a,int b)
+{
+    if(a>b)
+        return a;
+    return b;
 }
-int main() {
-    int n, W;
+
+int main()
+{
+    int n,W;
+
     printf("Enter number of items: ");
-    scanf("%d", &n);
-    int val[n], wt[n];
-    printf("Enter profit of items:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &val[i]);
+    scanf("%d",&n);
+
+    int wt[n], profit[n];
+
+    for(int i=0;i<n;i++)
+    {
+        printf("Weight and Profit of item %d: ",i+1);
+        scanf("%d%d",&wt[i],&profit[i]);
     }
-    printf("Enter weights of items:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &wt[i]);
+
+    printf("Enter capacity: ");
+    scanf("%d",&W);
+
+    int dp[n+1][W+1];
+
+    for(int i=0;i<=n;i++)
+    {
+        for(int j=0;j<=W;j++)
+        {
+            if(i==0 || j==0)
+                dp[i][j]=0;
+
+            else if(wt[i-1] <= j)
+            {
+                dp[i][j]=max(
+                    dp[i-1][j],
+                    profit[i-1]+dp[i-1][j-wt[i-1]]
+                );
+            }
+
+            else
+            {
+                dp[i][j]=dp[i-1][j];
+            }
+        }
     }
-    printf("Enter knapsack capacity: ");
-    scanf("%d", &W);
-    int result = knapsack(W, wt, val, n);
-    printf("\nMaximum value in Knapsack = %d\n", result);
+
+    printf("Maximum Profit = %d",dp[n][W]);
+
     return 0;
 }
