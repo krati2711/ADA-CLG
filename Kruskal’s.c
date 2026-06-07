dec9211b42eac918f77f0574c57266ab4b25f519
krati2711 <kratigoel.cs24@bmsce.ac.in>
2026-06-07 13:34:19 +0530

    Update Kruskal’s.c

--- Kruskal’s.c
+++ Kruskal’s.c
@@ -1,87 +1,89 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-struct Edge {
-    int src, dest, weight;
-};
+typedef struct{
+    int src;
+    int dest;
+    int weight;
+}Edge;
 
-struct Subset {
-    int parent;
-};
-
-int find(struct Subset subsets[], int i) {
-    while (subsets[i].parent != i)
-        i = subsets[i].parent;
+// find parent of a vertex
+int find(int parent[], int i){
+    while(parent[i]!=i)
+        i=parent[i];
     return i;
 }
 
-void Union(struct Subset subsets[], int x, int y) {
-    int xroot = find(subsets, x);
-    int yroot = find(subsets, y);
-    subsets[xroot].parent = yroot;
+// join two sets
+void Union(int parent[], int x, int y){
+    parent[x]=y;
 }
 
-int compare(const void *a, const void *b) {
-    return ((struct Edge *)a)->weight - ((struct Edge *)b)->weight;
-}
+int main(){
 
-void KruskalMST(struct Edge edges[], int V, int E) {
-    struct Edge result[V];
-    struct Subset subsets[V];
+    int V,E;
 
-    qsort(edges, E, sizeof(struct Edge), compare);
+    printf("enter no of vertices:");
+    scanf("%d",&V);
 
-    for (int i = 0; i < V; i++)
-        subsets[i].parent = i;
+    printf("enter no of edges:");
+    scanf("%d",&E);
 
-    int e = 0;
-    int i = 0;
+    Edge edge[E];
 
-    while (e < V - 1 && i < E) {
-        struct Edge next = edges[i++];
+    // input edges
+    for(int i=0;i<E;i++){
+        printf("enter src dest weight:");
+        scanf("%d%d%d",
+              &edge[i].src,
+              &edge[i].dest,
+              &edge[i].weight);
+    }
 
-        int x = find(subsets, next.src);
-        int y = find(subsets, next.dest);
+    // sort edges by weight
+    for(int i=0;i<E-1;i++){
+        for(int j=i+1;j<E;j++){
 
-        if (x != y) {
-            result[e++] = next;
-            Union(subsets, x, y);
+            if(edge[i].weight > edge[j].weight){
+
+                Edge temp=edge[i];
+                edge[i]=edge[j];
+                edge[j]=temp;
+            }
         }
     }
 
-    printf("\nEdges in MST:\n");
-    int total = 0;
+    int parent[V];
 
-    for (i = 0; i < e; i++) {
-        printf("%d -- %d == %d\n",
-               result[i].src,
-               result[i].dest,
-               result[i].weight);
-        total += result[i].weight;
+    // initially every vertex is its own parent
+    for(int i=0;i<V;i++)
+        parent[i]=i;
+
+    int count=0;
+    int cost=0;
+
+    printf("Edges in MST:\n");
+
+    for(int i=0;i<E && count<V-1;i++){
+
+        int x=find(parent,edge[i].src);
+        int y=find(parent,edge[i].dest);
+
+        if(x!=y){
+
+            printf("%d - %d = %d\n",
+                   edge[i].src,
+                   edge[i].dest,
+                   edge[i].weight);
+
+            cost+=edge[i].weight;
+
+            Union(parent,x,y);
+
+            count++;
+        }
     }
 
-    printf("Total Cost = %d\n", total);
-}
-
-int main() {
-    int V;
-
-    printf("Enter number of vertices: ");
-    scanf("%d", &V);
-
-    int E = V * (V - 1) / 2;
-    struct Edge edges[E];
-
-    printf("Enter edges (src dest weight):\n");
-
-    for (int i = 0; i < E; i++) {
-        scanf("%d %d %d",
-              &edges[i].src,
-              &edges[i].dest,
-              &edges[i].weight);
-    }
-
-    KruskalMST(edges, V, E);
+    printf("Total Cost = %d",cost);
 
     return 0;
 }
