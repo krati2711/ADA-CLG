d24dd0b0b19ff09c6b3db3f86404094f4c2cba20
krati2711 <kratigoel.cs24@bmsce.ac.in>
2026-06-07 13:00:46 +0530

    Update HeapSort.c

--- HeapSort.c
+++ HeapSort.c
@@ -1,60 +1,59 @@
 #include <stdio.h>
-void swap(int *a, int *b) {
-    int temp = *a;
-    *a = *b;
- *b = temp;
+
+void swap(int *a,int *b){
+    int temp=*a;
+    *a=*b;
+    *b=temp;
 }
-void siftUp(int a[], int i) {
-    while(i > 0) {
-        int parent = (i - 1) / 2;
-        if(a[parent] < a[i]) {
-            swap(&a[parent], &a[i]);
-            i = parent;
-        } else {
-            break;
-        }
+
+void heapify(int a[],int n,int i){
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+
+    if(left<n && a[left]>a[largest])
+        largest=left;
+
+    if(right<n && a[right]>a[largest])
+        largest=right;
+
+    if(largest!=i){
+        swap(&a[i],&a[largest]);
+        heapify(a,n,largest);
     }
 }
-void buildHeap(int a[], int n) {
-    for(int i = 1; i < n; i++) {
-        siftUp(a, i);
+
+void heapsort(int a[],int n){
+
+    for(int i=n/2-1;i>=0;i--)
+        heapify(a,n,i);
+
+    for(int i=n-1;i>0;i--){
+        swap(&a[0],&a[i]);
+        heapify(a,i,0);
     }
 }
-void heapify(int a[], int n, int i) {
-    int largest = i;
-    int left = 2*i + 1;
-    int right = 2*i + 2;
-    if(left < n && a[left] > a[largest])
-        largest = left;
-    if(right < n && a[right] > a[largest])
-        largest = right;
-    if(largest != i) {
-        swap(&a[i], &a[largest]);
-        heapify(a, n, largest);
-    }
-}
-void heapSort(int a[], int n) {
-    buildHeap(a, n);
-    for(int i = n-1; i > 0; i--) {
-        swap(&a[0], &a[i]);
-        heapify(a, i, 0);
-    }
-}
-void print(int a[], int n) {
-    for(int i = 0; i < n; i++)
-        printf("%d ", a[i]);
-    printf("\n");
-}
-int main() {
+
+int main(){
+
     int n;
-    printf("Enter n: ");
-    scanf("%d", &n);
+
+    printf("enter n:");
+    scanf("%d",&n);
+
     int a[n];
-    printf("Enter elements:\n");
-    for(int i = 0; i < n; i++)
-        scanf("%d", &a[i]);
-    heapSort(a, n);
-    printf("Sorted array:\n");
-    print(a, n);
+
+    printf("enter elements:");
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&a[i]);
+
+    heapsort(a,n);
+
+    printf("sorted array:\n");
+
+    for(int i=0;i<n;i++)
+        printf("%d ",a[i]);
+
     return 0;
 }
